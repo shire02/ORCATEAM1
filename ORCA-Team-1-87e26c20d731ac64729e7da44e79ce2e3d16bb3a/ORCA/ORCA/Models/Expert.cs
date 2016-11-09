b20c72cf70a516f37de863be54c86fc5f9026b87
Henok <henok_afro@yahoo,com>
2016-11-09 14:41:04 -0500

    change
    i made a change

--- ORCA-Team-1-87e26c20d731ac64729e7da44e79ce2e3d16bb3a/ORCA/ORCA/Models/Expert.cs
+++ ORCA-Team-1-87e26c20d731ac64729e7da44e79ce2e3d16bb3a/ORCA/ORCA/Models/Expert.cs
@@ -9,7 +9,7 @@ namespace ORCA.Models
     {
 
         //missing image and becameExpert flag for this table
-        // we may need photo
+        
         public int ID { get; set; }
         public string Title { get; set; }
         public string Category { get; set; }
